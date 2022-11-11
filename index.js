802bc74aeef3bc221a7e3c017e7e496c0a8b3553
Samer taha <alrazy1@gmail.com>
2022-11-11 13:49:27 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const colors = require('colors');
+const scraper = require('./scraper/scraper');
+
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+//const PORT = process.env.PORT || 8000;
+const PORT = 5000;
+connectDB();
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.get('/', function (req, res) {
+  res.send({ msg: 'hello' });
+});
+app.use('/api/wordBank', require('./routes/wordBankRoute'));
+app.use('/api/matches', require('./routes/userRoutes'));
+
+app.use(errorHandler);
+//scraper();
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
